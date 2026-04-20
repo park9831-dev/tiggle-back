bb531c80f7be11093d65131ea44ffb73f94b47ea
052.jinny.dev <052.jinny.dev@gmail.com>
2026-04-21 00:57:36 +0900

    `project init`

---
+++ src/main/java/com/tiggle/autotrading/AutotradingApplication.java
@@ -0,0 +1,13 @@
+package com.tiggle.autotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiggle/autotrading/AutotradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiggle.autotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
